f93cdda446ce351bf14e2941dd0f47d3fb1e544b
agudi24 <agudi24@gmail.com>
2019-05-24 16:31:02 +0000

    in progress of Activity 3

---
+++ areaofCircle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+areaofCircle(2.5);
+}
+
+
+float areaofCircle(float radius){
+float answer = ((radius * radius) * M_PI)
+
+return answer;
+
+}
+
+
+
