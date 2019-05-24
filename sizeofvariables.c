1e75fa120d9885858e2b3df458e0926cc1e63f24
agudi24 <agudi24@gmail.com>
2019-05-24 15:51:39 +0000

    made changes to sizeofvariables.c

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -2,12 +2,14 @@
 
 int main()
 {
+  int f = 78;
   double a = 6.99999;
   float b = 299.0;
   unsigned int c = 100000;
   signed int d = 123456789;
   char e = 'A';
   
+  printf("int f value %.4f and size %d bytes.\n", f, sizeof(f));
   printf("double a value %.4f and size %d bytes.\n", a, sizeof(a));
   printf("float b value %4.2f and size %d bytes.\n", b, sizeof(b));
   printf("unsigned int c value %d (oct %o, hex %x) and "		\
