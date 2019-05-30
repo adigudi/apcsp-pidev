eaff4357ed574dca155353309ac6d8b79dd16636
Adi Gudi <30880671+agudi24@users.noreply.github.com>
2019-05-30 08:17:15 -0700

    Update areaofcircle2.c

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 
-float areaOfCircle(float radius){
-float answer = ((radius * radius) * M_PI);
-return answer;
-
+//defining fuctions
+float areaOfCircle(float radius)
+{
+  float area = M_PI * radius * radius;
+  return area;
 }
 
-int main(){
-char input[256];
-char num1;
-char num2;
-printf("What is the radius of your circle?\n");
-fgets(input, 256, stdin);
-sscanf(input,"%s %s",&num1,&num2);
-float ans1 = areaOfCircle(atof(&num1));
-float ans2 = areaOfCircle(atof(&num2));
-printf("The area of the circle is %f\n", ans1);
-printf("The area of the circle is %f\n", ans2);
+void areaLoop(int arg, int argc, char* argv[])
+{
+  while (arg < argc)
+  {
+    float f = areaOfCircle(atof(argv[arg]));
+    printf("Area of circle with radius %s = %f\n", argv[arg], f);
+    arg++;
+  }
 }
+
+//start of code
+int main(int argc, char* argv[])
+{
+  if (argc > 1)
+  {
+    int arg = 1;
+    areaLoop(arg, argc, argv);
+  }
+  else
+  {
+    char input[256];
+    int arg = 0;
+    char arg1, arg2;
+    printf("Enter two radii to find the area of the two circles\n");
+    fgets(input, 256, stdin);
+    sscanf(input, "%s" "%s", &arg1, &arg2);
+    argv[0] = &arg1;
+    argv[1] = &arg2;
+    areaLoop(arg, 2, argv);
+  }
+  return 0;
